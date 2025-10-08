8f525f47e747e55dcc3b111c86db30c983515fc4
Struggler <sanpreethhost@gmail.com>
2025-10-09 02:55:46 +0530

    corrected imports

--- frontend/src/pages/SavedReports.jsx
+++ frontend/src/pages/SavedReports.jsx
@@ -12,6 +12,7 @@ gsap.registerPlugin(ScrollTrigger);
 const LoadingSpinner = () => (
     <div className="flex justify-center items-center p-8">
         <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-blue-400"></div>
+        
     </div>
 );
 
